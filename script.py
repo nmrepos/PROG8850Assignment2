dce65c9aebcab2d0aacdd1a10e0c3d1b03c6673e
nmrepos <nmurali1611@conestogac.on.ca>
2025-06-07 02:42:17 +0000

    Question 2

---
+++ script.py
@@ -0,0 +1,39 @@
+import mysql.connector
+import os
+import glob
+
+# Connect to MySQL
+connection = mysql.connector.connect(
+    host=os.environ['MYSQL_HOST'],
+    user=os.environ['MYSQL_USER'],
+    password=os.environ['MYSQL_PASSWORD'],
+    database=os.environ['MYSQL_DATABASE']
+)
+
+cursor = connection.cursor()
+
+# Find all SQL files in the sql directory
+sql_files = glob.glob('sql/*.sql')
+
+for sql_file in sql_files:
+    print(f"Executing {sql_file}")
+    with open(sql_file, 'r') as file:
+        sql_script = file.read()
+        
+    # Split script into commands
+    commands = sql_script.split(';')
+    
+    for command in commands:
+        command = command.strip()
+        if command:
+            print(f"Running: {command}")
+            cursor.execute(command)
+            
+# Commit changes
+connection.commit()
+
+# Close connection
+cursor.close()
+connection.close()
+
+print("All SQL scripts executed successfully!")
